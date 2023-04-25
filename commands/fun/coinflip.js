1ce9dd3716233dabfc436df4fe26f3fde6877d2a
xiAMLEO <leonardobonanni002@gmail.com>
2023-04-25 15:07:20 +0200

    added coinflip command
    the bot can now flip a coin and return either an head coin image or tails coin image

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,48 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  AttachmentBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("coinflip")
+    .setDescription("Toss a coin!"),
+  async execute(interaction) {
+    let file;
+    const embed = new EmbedBuilder()
+      .setImage("https://media.tenor.com/bd3puNXKLwUAAAAC/coin-toss.gif")
+      .setColor(0x6666ff)
+      .setTitle("Tossing a coin...")
+      .setAuthor({
+        name: `${interaction.user.username}`,
+        iconURL: interaction.user.displayAvatarURL(),
+      })
+      .setTimestamp();
+    await interaction.reply({ embeds: [embed] });
+    await sleep(2000);
+
+    const randomNumber = Math.random();
+    console.log(randomNumber);
+
+    if (randomNumber < 0.5) {
+      file = new AttachmentBuilder("assets\\head.png");
+      embed
+        .setImage("attachment://head.png")
+        .setTitle(`The result was head! 🪙`);
+    } else {
+      file = new AttachmentBuilder("assets\\tails.png");
+      embed
+        .setImage("attachment://tails.png")
+        .setTitle(`The result was tails! 🦅`);
+    }
+
+    await interaction.editReply({ embeds: [embed], files: [file] });
+  },
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
