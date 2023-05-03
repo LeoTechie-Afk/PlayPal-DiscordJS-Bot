9775214928e5e7254289fe0ff585f5d96732e7bd
xiAMLEO <leonardobonanni002@gmail.com>
2023-05-03 16:34:33 +0200

    Update filters.js
    The filters command is now complete with every subcommand (toggle, default, enabled, list)

--- commands/music/filters.js
+++ commands/music/filters.js
@@ -1,33 +1,103 @@
 const { useQueue } = require("discord-player");
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("filters")
     .setDescription("Toggles an ffmpeg filter on or off.")
-    .addStringOption((option) =>
-      option
-        .setName("filter_name")
-        .setDescription("The name of the filter you want to enable or disable")
-        .setRequired(true)
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("toggle")
+        .setDescription("Toggles an ffmpeg filter on or off.")
+        .addStringOption((option) =>
+          option
+            .setName("filter")
+            .setDescription("The name of the filter you want to toggle.")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("default")
+        .setDescription("Sets the filters to default")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("enabled")
+        .setDescription("Gives a list of all the enabled filters.")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("list")
+        .setDescription("Lists all the available filters")
     ),
   async execute(interaction) {
-    // TODO check input if it's valid if it isn't print a list of all the available filters
-    const filter_name = interaction.options.getString("filter_name");
+    // check which subcommand the user calls
     const queue = useQueue(interaction.guild.id);
     const channel = interaction.member.voice.channel;
 
-    if (!queue || !queue.isPlaying())
-      return await interaction.reply("🚫 The bot is not playing any songs");
     if (!channel)
       return await interaction.reply("🚫 User not in a voice channel");
+    if (!queue || !queue.isPlaying())
+      return await interaction.reply("🚫 The bot is not playing any songs");
 
-    // gets disabled filters
-    console.log(queue.filters.ffmpeg.getFiltersDisabled());
+    const filters = queue.filters.ffmpeg
+      .getFiltersDisabled()
+      .concat(queue.filters.ffmpeg.getFiltersEnabled());
 
-    // this is case sensitive
-    queue.filters.ffmpeg.toggle([filter_name]);
+    switch (interaction.options._subcommand) {
+      case "toggle":
+        // toggles a filter on or off
+        const filter_name = interaction.options.getString("filter");
 
-    await interaction.reply(`Toggled filter: ${filter_name}`);
+        if (!filters.includes(filter_name))
+          return await interaction.reply({
+            embeds: [
+              new EmbedBuilder()
+                .setTitle(
+                  `No result found for ${filter_name}! Here's a list of all the filters:`
+                )
+                .setDescription(filters.sort().join("\n"))
+                .setColor(0xe50000),
+            ],
+          });
+
+        queue.filters.ffmpeg.toggle([filter_name]);
+
+        await interaction.reply(`Toggled filter: **${filter_name}**`);
+        break;
+      case "default":
+        // sets the filters to their default value
+        queue.filters.ffmpeg.toggle(queue.filters.ffmpeg.getFiltersEnabled());
+        await interaction.reply("Setting the filters back to default");
+        break;
+      case "enabled":
+        // gives a list of all the enabled commands
+        await interaction.reply(
+          queue.filters.ffmpeg.getFiltersEnabled().length > 0
+            ? {
+                embeds: [
+                  new EmbedBuilder()
+                    .setTitle("Here's a list of all the enabled filters!")
+                    .setDescription(
+                      queue.filters.ffmpeg.getFiltersEnabled().join("\n")
+                    )
+                    .setColor(0x39ffef),
+                ],
+              }
+            : "🚫 There are no filters currently enabled!"
+        );
+        break;
+      default:
+        // list all the filters
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle(`Here's a list of all the filters:`)
+              .setDescription(filters.sort().join("\n"))
+              .setColor(0xe50000),
+          ],
+        });
+    }
   },
 };
