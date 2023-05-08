f42884bea30742790b63ada3cb0ea48f7750c63d
xiAMLEO <leonardobonanni002@gmail.com>
2023-05-08 21:59:59 +0200

    Update search.js

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { useMasterPlayer, QueryType } = require("discord-player");
-
+const ytdl = require("ytdl-core");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("search")
